7001b55df6731c45f4676fdf8eeea4f2d7c3c829
Yash Patil <yashpatil063@gmail.com>
2022-02-24 16:34:00 +0530

    Multithreaded project commit Demo for practice

---
+++ TestSynchronization/src/WithoutSynchronization/SyncDemo.java
@@ -0,0 +1,60 @@
+package WithoutSynchronization;
+
+class Sender
+{
+	public synchronized void send(String msg)
+	{
+		System.out.println("Sending\t "+ msg);
+		try {
+			Thread.sleep(1000);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Thread interupted.");
+		}
+		System.out.println("\n "+msg+" Send");
+	}
+}
+
+class ThreadSend extends Thread{
+	
+	private String msg;
+	Sender sender;
+	
+	ThreadSend(String m, Sender obj)
+	{
+		msg = m;
+		sender = obj;
+	}
+	
+	public void run()
+	{
+		synchronized (sender) 
+		{
+			sender.send(msg);
+		}
+	}
+}
+
+
+public class SyncDemo 
+{
+	public static void main(String[] args) {
+		Sender snd = new Sender();
+		ThreadSend s1 = new ThreadSend(" Hi ",snd);
+		
+		ThreadSend s2 = new ThreadSend(" Bye ", snd);
+		
+		s1.start();
+		s2.start();
+		
+		try {
+			s1.join();
+			s2.join();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Interrupted");
+		}
+	}	
+}
